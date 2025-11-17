8e5d37b752a27a656197b7ea9997d173d4f43536
ben-ceh <b3niib@gmail.com>
2025-11-17 13:09:06 +0100

    backend zold

---
+++ backend/backend.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+const pool = mysql.createPool({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'okos_kozzoseg'
+        })
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+//Bence vépontjai
+app.get('/olvaso', (req, res) => {
+        const sql=`SELECT * from felhasznalok`
+        pool.query(sql, (err, result) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).json({error:"Hiba"})
+        }
+        if (result.length===0){
+            return res.status(404).json({error:"Nincs adat"})
+        }
+
+        return res.status(200).json(result)
+        })
+})
+//Sanyi vépontjai
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
