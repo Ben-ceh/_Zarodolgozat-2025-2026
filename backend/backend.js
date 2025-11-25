00e258d7edcd23955732a497495193d6743e6ac0
ben-ceh <b3niib@gmail.com>
2025-11-25 08:45:12 +0100

    bejKepek,felhKepek,

--- backend/backend.js
+++ backend/backend.js
@@ -6,6 +6,9 @@ const port = 3000
 
 app.use(cors())
 app.use(express.json())
+app.use("/kepek",express.static("kepek"))
+app.use("/kepekFelhasznalo",express.static("kepekFelhasznalo"))
+app.use("/bejegyzesKepek",express.static("bejegyzesKepek"))
 
 const pool = mysql.createPool({
         host: 'localhost',
@@ -13,6 +16,10 @@ const pool = mysql.createPool({
         password: '',
         database: 'okos_kozosseg'
         })
+
+// const formatDate = (mysqlDate) => {
+//   return mysqlDate.split("T")[0]; // "2025-11-17"
+// }; {formatDate(elem.letrehozva)} 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -33,6 +40,45 @@ app.get('/felhasznaloim', (req, res) => {
         return res.status(200).json(result)
         })
 })
+
+app.post('/bejegyzesekKeresId', (req, res) => {
+        const {bejegyzesek_id} =req.body
+        const sql=`
+                select *
+                from bejegyzesek
+                inner join felhasznalok
+                on felhasznalo_id=felhasznalok_id
+                where bejegyzesek_id=?
+                `
+        pool.query(sql,[bejegyzesek_id], (err, result) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).json({error:"Hiba"})
+        }
+        if (result.length===0){
+            return res.status(404).json({error:"Nincs adat"})
+        }
+
+        return res.status(200).json(result)
+        })
+})
+app.get('/bejegyEsFelh', (req, res) => {
+        const sql=`SELECT * from bejegyzesek 
+        inner JOIN felhasznalok 
+        on bejegyzesek.felhasznalo_id = felhasznalok.felhasznalok_id;`
+        pool.query(sql, (err, result) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).json({error:"Hiba"})
+        }
+        if (result.length===0){
+            return res.status(404).json({error:"Nincs adat"})
+        }
+
+        return res.status(200).json(result)
+        })
+})
+
 //Sanyi végpontjai
 app.get('/bejegyzesek', (req, res) => {
         const sql=`SELECT * from bejegyzesek`
